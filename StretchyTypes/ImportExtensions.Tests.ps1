d877edd0978dcf627daaa10bf42be2b497c5cff6
carrvo <carrvo@gmail.com>
2024-01-26 17:24:27 -0700

    added starting functional test

--- StretchyTypes/ImportExtensions.Tests.ps1
+++ StretchyTypes/ImportExtensions.Tests.ps1
@@ -1,4 +1,16 @@
 using module .\bin\Debug\net7.0\ImportExtensions.dll
+#using assembly .\bin\Debug\net7.0\ImportExtensions.UnitTests.dll
 
 Describe "ImportExtensions" {
+	Context "When Importing" {
+		$assembly = [System.Reflection.Assembly]::LoadFile($(Get-Item '.\bin\Debug\net7.0\ImportExtensions.UnitTests.dll' | Select-Object -ExpandProperty FullName))
+		Import-Extensions -Assembly $assembly
+
+		It "Maps Extension" {
+			<#Get-TypeData -TypeName ImportExtensions.UnitTests.ExampleClass |
+				Select-Object -ExpandProperty Members#>
+			$example = New-Object ImportExtensions.UnitTests.ExampleClass
+			$example.ExtensionMethod("me") | Should Be "Hello me from ExtensionMethod"
+		}
+	}
 }
