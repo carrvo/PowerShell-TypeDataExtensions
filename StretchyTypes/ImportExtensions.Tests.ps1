c3fd8cab00b3c44598b99ec4486591e3b81f95da
carrvo <carrvo@gmail.com>
2024-01-31 15:38:57 -0700

    Check TypeData FTs

--- StretchyTypes/ImportExtensions.Tests.ps1
+++ StretchyTypes/ImportExtensions.Tests.ps1
@@ -4,16 +4,36 @@ Describe "ImportExtensions" {
 	Context "When Importing" {
 		Import-Extensions -Path '.\bin\Debug\net7.0\ImportExtensions.UnitTests.dll'
 
+		It "Updates TypeData for Concrete Extension" {
+			Get-TypeData -TypeName ImportExtensions.UnitTests.ExampleClass |
+				Select-Object -ExpandProperty Members |
+				Select-Object -ExpandProperty Keys |
+				Should -Be "ExtensionMethod"
+		}
+
 		It "Maps Concrete Extension" {
-			<#Get-TypeData -TypeName ImportExtensions.UnitTests.ExampleClass |
-				Select-Object -ExpandProperty Members#>
 			$example = New-Object ImportExtensions.UnitTests.ExampleClass
 			$example.ExtensionMethod("me") | Should -Be "Hello me from ExtensionMethod"
+		}
+
+		It "Updates TypeData for Interface Extension" {
+			Get-TypeData -TypeName ImportExtensions.UnitTests.IExampleClass |
+				Select-Object -ExpandProperty Members |
+				Select-Object -ExpandProperty Keys |
+				Should -Be "ExtensionIMethod"
 		}
 
 		It "Maps Interface Extension" {
 			$example = New-Object ImportExtensions.UnitTests.ExampleClass
 			$example.ExtensionIMethod("me") | Should -Be "Hello me from ExtensionIMethod"
+		}
+
+		It "Updates TypeData for Generic Extension" {
+			Get-TypeData |
+				Where-Object TypeName -Match ImportExtensions.UnitTests.Generic.ExampleClass |
+				Select-Object -ExpandProperty Members |
+				Select-Object -ExpandProperty Keys |
+				Should -Be "ExtensionMethod"
 		}
 
 		It "Maps Generic Extension" {
